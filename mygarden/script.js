e06bce4dce53d0db39ac42061bd0bf240fd8ed8f
dawnsink <60200649+dawnsink@users.noreply.github.com>
2021-03-01 17:26:40 -0500

    cursor update

---
+++ mygarden/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#line1").click(function(){
+    	$("#line1v2").show();
+    	 });
+
+  // $("#pup").click(function(){
+  //     $("#t1").hide();
+  //       });
+  //
+  //     $("error").hover(function(){
+  //             $(this).hide();
+  //             });
+  //
+  //               $("#t3").hover(function(){
+  //                         $("#t2").append(" and your ghost");
+  //                         });
+
+})
