cb76c82e61245d9944a1e64ffca0eda90bbca460
dawnsink <60200649+dawnsink@users.noreply.github.com>
2021-03-01 01:04:37 -0500

    adding jquery

---
+++ tryagain/moveit.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $("#t1").draggable();
+  $("#baby").draggable();
+  // $("#error").draggable();
+
+  $("#t2").click(function(){
+    	$("#video").show();
+    	 });
+
+  // $("#pup").hover(function(){
+  $("#pup").click(function(){
+      $("#t1").hide();
+        });
+
+      $("error").hover(function(){
+              $(this).hide();
+              });
+
+              // $("#t3").hover(function(){
+              //   $("#t2").css("color", "red");
+              //   });
+
+                $("#t3").hover(function(){
+                          $("#t2").append(" and your ghost");
+                          });
+
+  // alert("hello world");
+})
+
+
+// $(document).ready(function(){
+//   alert("hihihihi");
+// })
