6b02cf85e1541f6e7730fc1228926108e052e120
dawnsink <60200649+dawnsink@users.noreply.github.com>
2021-04-05 18:17:15 -0400

    jquery working!

---
+++ stuffstuffstuff/jquery.js
@@ -0,0 +1,9 @@
+window.setInterval(function(){
+
+  var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+
+   $('body').css({
+ 'background-color' : randomColor,
+});
+
+}, 2000);
