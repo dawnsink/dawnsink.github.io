1feaae2f5bc0cb1a0e5173e95c504a5e1d245a31
dawnsink <60200649+dawnsink@users.noreply.github.com>
2021-04-06 14:18:30 -0400

    constrain color shift for legibility/text contrast

--- stuffstuffstuff/jquery.js
+++ stuffstuffstuff/jquery.js
@@ -1,9 +1,20 @@
 window.setInterval(function(){
 
-  var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+  var randomH = Math.floor(Math.random() * 360);
 
    $('body').css({
- 'background-color' : randomColor,
+ 'background-color' : 'hsl(' + randomH + ', 100%, 70%)'
 });
 
-}, 30000);
+}, 1000);
+
+
+// window.setInterval(function(){
+//
+//   var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+//
+//    $('body').css({
+//  'background-color' : randomColor,
+// });
+//
+// }, 30000);
